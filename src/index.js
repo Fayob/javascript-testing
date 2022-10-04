323599fc3de4333ee902be060938e07421795ce6
= <adedejifavour6@gmail.com>
2022-10-04 22:29:15 +0100

    Add calculator class and it's test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 // Test 1
-const stringLength = (string) => string.length;
+const stringLength = (string) => {
+  if (string.length < 1 || string.length > 10) {
+    throw new Error('Invalid Input');
+  }
+  return string.length;
+};
 
 // Task 2
 const reverseString = (string) => string.split('').reverse().join('');
 
-module.exports = { stringLength, reverseString };
+class Calculator {
+  constructor(firstValue, secondValue) {
+    this.firstValue = firstValue;
+    this.secondValue = secondValue;
+  }
+
+  add() {
+    return this.firstValue + this.secondValue;
+  }
+
+  subtract() {
+    return this.firstValue - this.secondValue;
+  }
+
+  multiply() {
+    return this.firstValue * this.secondValue;
+  }
+
+  divide() {
+    return this.firstValue / this.secondValue;
+  }
+}
+
+module.exports = { stringLength, reverseString, Calculator };
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,36 @@
-const { stringLength, reverseString } = require('./index.js');
+const { stringLength, reverseString, Calculator } = require('./index.js');
 
 // Task 1
 it('should return the length value', () => {
   expect(stringLength('game')).toBe(4);
 });
+test('throw error if string is empty', () => {
+  expect(() => stringLength('')).toThrow(Error);
+});
+test('throw error if string is more than 10', () => {
+  expect(() => stringLength('qwertyuiopasdfg')).toThrow(Error);
+});
 
 // Task 2
 it('should reverse entered string', () => {
   expect(reverseString('age')).toBe('ega');
+});
+it('should reverse entered string', () => {
+  expect(reverseString('again')).toBe('niaga');
+});
+
+// Task 3
+describe('calculator', () => {
+  test('should return the addition of two values', () => {
+    expect(new Calculator(5, 5).add()).toBe(10);
+  });
+  test('should return the subtraction of two values', () => {
+    expect(new Calculator(6, 4).subtract()).toBe(2);
+  });
+  test('should return the addition of two values', () => {
+    expect(new Calculator(4, 25).multiply()).toBe(100);
+  });
+  test('should return the addition of two values', () => {
+    expect(new Calculator(15, 5).divide()).toBe(3);
+  });
 });
